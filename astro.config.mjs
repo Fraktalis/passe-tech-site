c4cfc3e94549deb81818258f864fcc77c2fb2579
Alexandre VINCENT <mrfraktalis@gmail.com>
2026-03-25 23:01:14 +0100

    fix: Added missing slash to dynamic urls

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://fraktalis.github.io',
-  base: '/passe-tech-site',
+  base: '/passe-tech-site/',
   output: 'static',
 });
